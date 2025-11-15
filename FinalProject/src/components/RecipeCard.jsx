c85b7d1b235b41100f5fa5f643323f2bcb3aff79
Anastasia <anast111antonova@gmail.com>
2025-11-15 12:03:45 +0200

    Add RecipeCard

---
+++ FinalProject/src/components/RecipeCard.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const RecipeCard = ({recipe}) => {
+	const navigate = useNavigate();
+
+	return (
+		<div className="recipe-card" onClick={() => navigate('/recipe/${recipe.idMeal')}>
+			<img src="{recipe.strMealThumb}" alt="{recipe.strMeal}" />
+			<div className="info">
+				<h3>{recipe.strMeal}</h3>
+				<p>{recipe.strCategory}</p>
+			</div>
+		</div>
+	)
+}
+
+export default RecipeCard
