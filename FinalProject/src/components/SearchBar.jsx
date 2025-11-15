8ee408fb48c057ad92fc0c041f8ad4ee27f1c246
Anastasia <anast111antonova@gmail.com>
2025-11-15 12:07:49 +0200

    Add Search bar

---
+++ FinalProject/src/components/SearchBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SearchBar = ({search, setSearch}) => {
+	return (
+		<div className="search-bar">
+			<input type="text" placeholder='Search recipe' value={search} onChange={(e) => setSearch(e.target.value)}/>
+		</div>
+	)
+}
+
+export default SearchBar
